20dbe81e50452622335104dfb50ec6dc5d0d2341
RayaXraya <rrayanmr2003@gmail.com>
2023-09-13 02:57:09 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Printing alphabet ten
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char d;
+	int i;
+
+	for ( i = 0; i < 10; i++)
+	{
+		for ( d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+		_putchar('\n');
+	}
+}	
