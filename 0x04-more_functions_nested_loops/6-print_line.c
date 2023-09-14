c1b0ef42d2d80d52b01acc9a08ae09534f8c4306
RayaXraya <rrayanmr2003@gmail.com>
2023-09-14 11:39:31 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line in the terminal
+ * @n: num
+ * Return: ...
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int z;
+
+		for (z = 0; z == n; z++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
