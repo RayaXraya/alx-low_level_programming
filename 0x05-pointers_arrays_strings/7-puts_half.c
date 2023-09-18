7f5f851823ba7936a9a16497cea4091ac9b977e7
RayaXraya <rrayanmr2003@gmail.com>
2023-09-18 13:09:19 -0700

    l

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string
+ * Return: ...
+ */
+
+void puts_half(char *str)
+{
+	int i, l;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	if (i % 2 == 0)
+	{
+		for (l = i / 2; str[l] != '\0'; l++)
+		{
+			_putchar(str[l]);
+		}
+	}
+	else
+	{
+		for (l = i / 2 + 2; str[l] != '\0'; l++)
+			_putchar(str[l]);
+	}
+	_putchar('\n');
+}
+
