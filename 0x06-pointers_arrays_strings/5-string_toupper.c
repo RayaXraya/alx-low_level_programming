4e31185ee3f04e38c050d19cf73927a33d8a8037
RayaXraya <rrayanmr2003@gmail.com>
2023-09-20 11:32:36 -0700

    l

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Change lowercase to upper
+ * @str: The string
+ * Return: The new string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
