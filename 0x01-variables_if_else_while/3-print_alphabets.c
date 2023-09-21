4c2a05af9ef9202f75accde2bd441fe63f8ac80a
RayaXraya <rrayanmr2003@gmail.com>
2023-09-21 12:23:30 -0700

    l

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Laaa
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c, k;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (k = 'A'; k <= 'Z'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
