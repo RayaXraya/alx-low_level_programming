ac5b3a3ba6e30ffdddceed22e097bbe3ca73fd90
RayaXraya <rrayanmr2003@gmail.com>
2023-09-21 12:54:22 -0700

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Laaam
+ * Description: Lhhh
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
