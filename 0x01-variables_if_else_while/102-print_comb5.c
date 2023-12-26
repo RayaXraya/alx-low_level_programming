04334b687891c0847f1974a2c7138a92dfa276a7
RayaXraya <rrayanmr2003@gmail.com>
2023-12-25 23:12:37 -0800

    l

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: Lggg
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (k = i; k <= 99; k++)
+		{
+			if (i != k)
+			{
+				putchar(i / 10 + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar(k / 10 + 48);
+				putchar((k % 10) + 48);
+			}
+			if (!(i == 98 || k == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
