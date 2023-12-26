00f811904fb25ad770c015f59648ea2289cb7834
RayaXraya <rrayanmr2003@gmail.com>
2023-12-25 23:14:38 -0800

    l

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,10 +16,10 @@ int main(void)
 		{
 			if (i != k)
 			{
-				putchar(i / 10 + 48);
+				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar(k / 10 + 48);
+				putchar((k / 10) + 48);
 				putchar((k % 10) + 48);
 			}
 			if (!(i == 98 || k == 99))
