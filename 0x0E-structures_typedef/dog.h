6893de49ed7e8af5cba91d115105282e585c21fc
RayaXraya <rrayanmr2003@gmail.com>
2024-01-23 05:47:52 -0800

    l

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOGGY
 #define DOGGY
 
-struct dog {
+/**
+ * struct dog - stores info about dogs
+ * @name: dog name
+ * @age: its age
+ * @owner: the owner
+ *
+ * Description: can be used for any dog
+ */
+
+struct dog 
+{
 	char *name;
 	float age;
 	char *owner;
